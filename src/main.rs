f672860bafa86db3c290ac0659ab8f086534a6de
German Nikolishin <german.nikolishin@gmail.com>
2022-06-04 18:57:24 +0100

    basic hello world iced app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Sandbox, Container, Text, Settings, Length};
+
+fn main() -> iced::Result {
+    println!("Starting GUI");
+    IcedPFS::run(Settings::default())
+}
+
+struct IcedPFS { }
+
+impl Sandbox for IcedPFS {
+    type Message = ();
+
+    fn new() -> Self {
+        IcedPFS { }
+    }
+
+    fn title(&self) -> String {
+        "IcedPFS".into()
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        
+    }
+
+    fn view(&mut self) -> iced::Element<Self::Message> {
+        Container::new(Text::new("Hello, World"))
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
 }
