30fb50c411be03d1eb8d73533a20ac5ab272f5d9
German Nikolishin <german.nikolishin@gmail.com>
2022-06-07 22:29:19 +0100

    launching ipfs node during object creation

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ mod ipfs_client;
 
 use gui::IcedPFS;
 
+use crate::ipfs_client::Operations;
+
 
 fn main() -> iced::Result {
-    println!("Starting GUI");
-    let body = ipfs_client::request_files();
-    if let Ok(b) = body {
-        println!("{}", b);
+    let created_client = ipfs_client::Client::default();
+    if let Ok(client) = created_client {
+        let body = client.list_files();
+        println!("{:?}", body);
     }
+    
+    println!("Starting GUI");
     IcedPFS::run(Settings::default())
 }
